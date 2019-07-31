9797776074f9746915cfc33c214cd998c41b304f
emilygalo <53440449+emilygalo@users.noreply.github.com>
2019-07-31 10:13:43 -0400

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("Hello my name is Emily!")
+print("I Like to play tennis")
+
+5*2
+5/2
+5**2
+6**5
+
+def multiply (a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(4,5)
+
+
+    
---
+++ Day 2.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:50:39 2019
+
+@author: STEM
+"""
+
+def cadbury(a):
+    cadbury = a+10
+    print(cadbury)
+    
+cadbury(5)
+
+def cadbury(a):
+    cadbury = (10)+a
+    print(cadbury)
+    
+cadbury(5)
+
+def cadbury(a):
+    cadbury = a+5
+    print(cadbury)
+    
+cadbury(15)
+
+def cadburybox(a,b):
+    cadburybox = a+(5),b+(5)
+    print(cadburybox)
+    
+cadburybox(10,15)
+
+def cadburybox(a,b,c):
+    print("There are 3 types of chocolate in the cadbury box,", a,",", b,", and", c,)
+    
+cadburybox("White Chocolate", "Milk Chocolate", "Dark Chocolate")
+
+
+print("Hello Emily")
+
+a="Emily"
+print("Hello",a)
+
+name = input("Please enter your name.")
+
+print("Your name is", name,",Hello", name) 
+
+age = input("Please enter your age.")
+
+print("You're", age,"correct?")
+
+import math
+
+dir(math)
+
+math.pi
+
+math.factorial(12)
+
+math.factorial(0)
+
+math.pow(6,15)
+
+
+def cubicroot (a):
+    print("The cubic root of", a,"is",math.pow(a,(1/3)))
+
+cubicroot(20)
+
+
+a = int(input("Enter the value for the cubic root."))
+
+print("Hello! Welcome to the cubic root calculator.")
+cubicroot = input("Enter the value for the cubic root.")
+
+def value (a):
+    print("The cubic root of", a,"is",math.pow(a,(1/3)))
+
+value(a)
+
+ageint = int(age)
+
+
+
+
