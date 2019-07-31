96a09820bc20627a90c86d24d6b3c5ca1a21bfc4
emilygalo <53440449+emilygalo@users.noreply.github.com>
2019-07-31 11:56:23 -0400

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 10:50:14 2019
+
+@author: STEM
+"""
+
+Darkchocolate = 5
+Milk = 6
+White = 8
+print(White)
+White
+
+CadburyType1 = 6
+CadburyType2= 5
+CadburyType3= 8
+print("There are", CadburyType1,"Milk choclolates,", CadburyType2,"Dark chocolates, and", CadburyType3,"White chocolates in the Cadbury Box.")
+
+Chocolate1 ={"Cadburymilk":5} 
+Chocolate2 ={"Cadburydark":8}
+Chocolate3 ={"Cadburywhite":3}
+print(Chocolate1,Chocolate2,Chocolate3)
+
+Steve ={"Steve":32}
+Lia ={"Lia":28}
+Vin ={"Vin":45}
+Katie ={"Katie":38}
+print(Steve, Lia, Vin, Katie)
+
+studentage ={"Steve":32,"Lia":28,"Vin":45,"Katie":38}
+studentage
+
+studentgender ={"Steve":"M","Lia":"F","Vin":"M","Katie":"F"}
+studentgender
+
+student =[studentage,studentgender]
+student
+
+
+studentlist = [['Steve',32,"M"],["Lia",28,"F"],["Vin",45,"M"],["Katie",38,"F"]]
+studentlist
+
+import pandas
+dir(pandas)
+
+studentdf = pandas.DataFrame(studentlist)
+studentdf
+
+chocolates = [["Milk", 5], ["Dark", 6], ["White"]]
+
+
+
+
+
+
+
+
+
+
+
+
+
